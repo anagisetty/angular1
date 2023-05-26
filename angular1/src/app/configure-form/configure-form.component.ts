0673673be7a648c8b9bd2b6e00a3a1ee153e4b87
dotnet-code <@dotnet-code>
2023-05-26 11:26:04 +0530

    commit from code202305262604

---
+++ angular1/src/app/configure-form/configure-form.component.ts
@@ -0,0 +1,39 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-configure-form',
+  templateUrl: './configure-form.component.html',
+  styleUrls: ['./configure-form.component.css']
+})
+export class ConfigureFormComponent implements OnInit {
+
+  configureForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.configureForm = this.formBuilder.group({
+      jiraUsername: ['', Validators.required],
+      jiraPassword: ['', Validators.required],
+      jiraURL: ['', Validators.required],
+      repositoryName: ['', Validators.required]
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.configureForm.controls; }
+
+  onFormSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.configureForm.invalid) {
+        return;
+    }
+    alert('Form Submitted Successfully');
+  }
+
+}
